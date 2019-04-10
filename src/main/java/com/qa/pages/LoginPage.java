c35d191f814e6d54cd8ddfcfb468338a38929b29
srinusoftb <srinusoftb@gmail.com>
2019-04-10 19:08:47 -0400

    Modified Login page and added new Searchpage

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,5 +5,9 @@ public class LoginPage {
 	public void loginPage(){
 		System.out.println("Login Page Method");
 	}
+	
+	public void loginSetup(){
+		System.out.println("Loging page Setup page method");
+	}
 
 }
